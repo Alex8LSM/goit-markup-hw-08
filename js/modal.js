db09c280cd4c86b1af0a965f933316b259bcb486
Oleksandr Khvaleba <alex8inbox@gmail.com>
2021-09-05 14:26:19 +0300

    Adaptive

--- js/modal.js
+++ js/modal.js
@@ -4,7 +4,17 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
+    const toggleMenu = () => {
+      const isMenuOpen = refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
+      refs.openModalBtn.setAttribute('aria-expanded', !isMenuOpen);
 
+      const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
+    };
+
+  refs.openModalBtn.addEventListener('click', toggleMenu);
+  refs.closeModalBtn.addEventListener('click', toggleMenu);
+  
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
